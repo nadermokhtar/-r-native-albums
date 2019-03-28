11e0d79a7285bf195cbd4a9321dd241e0fc11ead
nadermokhtar <nader.mokhtar@gmail.com>
2019-03-28 15:48:48 -0400

    second commit

---
+++ src/components/AlbumList.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { View, Text } from "react-native";
+import axios from "axios";
+export default class AlbumList extends React.Component {
+
+    state = {
+      albums:[]
+    }
+
+  componentWillMount() {
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => console.log(response));
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>Album List !!!</Text>
+      </View>
+    );
+  }
+}
